783e76d1858fd0092e53a14118e7a4c6db00c67f
ZachTheMighty <promoted66@gmail.com>
2026-09-25 18:00:34 +0300

    fix: validate result middleware not returning

--- middlewares/validate_result.js
+++ middlewares/validate_result.js
@@ -2,6 +2,7 @@ const { validationResult } = require("express-validator");
 
 module.exports = (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) res.status(422).json({ errors: errors.array() });
+  if (!errors.isEmpty())
+    return res.status(422).json({ errors: errors.array() });
   next();
 };
